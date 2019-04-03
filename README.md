1d6ade1cb3673193cf44dae062cfdf6bd2dabf2d
jfkode <josefcoperez@outlook.com>
2019-04-03 22:20:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# LarryGithub
---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{'name': 'To-do Application',
+    'description': 'Manage personal to-do tasks.',
+    'author': 'José Francisco Pérez',
+    'depends': ['base'],
+    'data': ['views/todo_menu.xml',
+             'views/todo_view.xml'],
+    'application': True,
+ }
